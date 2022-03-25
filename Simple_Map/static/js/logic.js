384eb691aa7a981f4e303c916c24135b18e69ef1
matt Rickle <matt.rickle@outlook.com>
2022-03-25 19:44:20 -0400

    update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,6 +3,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
+//var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -13,6 +14,14 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   });
 
 // We create the tile layer that will be the background of our map.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+  radius: 100,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
